e4d95ffd9eea0bdbc3017307f463f3c957a90577
heyimmatt <mrmcfarland@gmail.com>
2020-09-28 11:00:16 -0700

    Complete makeText function and add tests

--- markov.js
+++ markov.js
@@ -31,7 +31,22 @@ class MarkovMachine {
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    const text = [];
+    const wordChoices = Object.keys(this.chains);
+
+    let randStartIndex = Math.floor(Math.random() * wordChoices.length);
+    let currWord = wordChoices[randStartIndex];
+    text.push(currWord);
+
+    while(text.length < numWords) {
+      let nextRandIndex = Math.floor(Math.random() * this.chains[currWord].length);
+      let nextWord = this.chains[currWord][nextRandIndex];
+      if (nextWord){
+        text.push(nextWord);
+        currWord = nextWord;
+      } else break;
+    }
+    return text.join(' ');
   }
 }
 
